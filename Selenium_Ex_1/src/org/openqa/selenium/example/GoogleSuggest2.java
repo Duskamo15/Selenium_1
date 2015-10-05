f3aa9d47aa51ddb250aaddedfa723f5be8bc1b25
Duskamo15 <dustin.landry@perficient.com>
2015-10-05 10:44:37 -0500

    simple git test

--- Selenium_Ex_1/src/org/openqa/selenium/example/GoogleSuggest2.java
+++ Selenium_Ex_1/src/org/openqa/selenium/example/GoogleSuggest2.java
@@ -3,6 +3,7 @@ package org.openqa.selenium.example;
 import java.util.List;
 import java.util.Scanner;
 
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
